75803cbc7457e30af015ef73343e02669336bf85
Henry Koleoso <henry.koleoso@featurespace.co.uk>
2022-07-22 15:34:12 +0100

    add dice command

--- lib/cogs/fun.py
+++ lib/cogs/fun.py
@@ -1,3 +1,5 @@
+from random import choice
+from random import randint
 from discord.ext.commands import Cog
 from discord.ext.commands import command
 
@@ -6,9 +8,20 @@ class Fun(Cog):
     def __init__(self, bot):
         self.bot = bot
 
-    @command(name="hello", aliases=["hi", "hey", "sup", "hiya", "greetings", "Hi", "Hey", "Sup", "Hiya", "Greetings"])
+    @command(name="hello", aliases=["hi", "hey", "sup", "hiya", "greetings", "Hi", "Hey", "Sup", "Hiya", "Greetings", "Hola",
+                                    "Bonjour", "Guten tag", "hola", "bonjour", "guten tag"])
     async def say_hello(self, ctx):
-        await ctx.send(f"Hello {ctx.author.mention}!")
+        await ctx.send(f"{choice(('Hey,', 'Hi,', 'Sup,', 'Hiya,', 'Greetings,', 'Bonjour', 'Guten tag', 'Hola'))}"
+                       f" {ctx.author.mention}")
+
+    # Dice Example command: 1d6 - rolls 1 dice, with highest value being 6
+    @command(name="dice", aliases=["roll"])
+    async def roll_dice(self, ctx, die_string: str):
+        dice, value = (int(term) for term in die_string.split("d"))
+        rolls = [randint (1, value) for i in range(dice)]
+
+        await ctx.send(" + ".join([str(r) for r in rolls]) + f" = {sum(rolls)}")
+
 
     @Cog.listener()
     async def on_ready(self):
