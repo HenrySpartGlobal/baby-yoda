ebd7b6e84463cfac4e0ae46aad683976c224b6d6
Henry Koleoso <henry.koleoso@featurespace.co.uk>
2022-07-22 14:17:09 +0100

    add greet command

--- lib/cogs/fun.py
+++ lib/cogs/fun.py
@@ -1,9 +1,14 @@
 from discord.ext.commands import Cog
+from discord.ext.commands import command
 
 
 class Fun(Cog):
     def __init__(self, bot):
         self.bot = bot
+
+    @command(name="hello", aliases=["hi", "hey", "sup", "hiya", "greetings", "Hi", "Hey", "Sup", "Hiya", "Greetings"])
+    async def say_hello(self, ctx):
+        await ctx.send(f"Hello {ctx.author.mention}!")
 
     @Cog.listener()
     async def on_ready(self):
