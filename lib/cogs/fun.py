e37a928d5b764e290b281814034eb815e9ed910a
Henry <Henry_Koleoso@hotmail.co.uk>
2022-07-23 00:25:03 +0100

    add baby yoda gifs to greeting command

--- lib/cogs/fun.py
+++ lib/cogs/fun.py
@@ -15,10 +15,14 @@ class Fun(Cog):
     @command(name="hello",
              aliases=["hi", "hey", "sup", "hiya", "greetings", "Hi", "Hey", "Sup", "Hiya", "Greetings", "Hola",
                       "Bonjour", "Guten tag", "hola", "bonjour", "guten tag"])
+    # cooldown - 3 allow in 1 minute
+    @cooldown(3, 60, BucketType.guild)
     async def say_hello(self, ctx):
         async with ctx.channel.typing():
             await ctx.send(f"{choice(('Hey,', 'Hi,', 'Sup,', 'Hiya,', 'Greetings,', 'Bonjour', 'Guten tag', 'Hola'))}"
                            f" {ctx.author.mention}")
+            await ctx.send(
+                f"{choice(('https://tenor.com/view/the-mandalorian-baby-yoda-the-child-star-wars-cute-gif-16181871', 'https://tenor.com/view/baby-yoda-hi-hello-greet-wave-gif-15912640', 'https://tenor.com/view/the-mandalorian-the-child-baby-yoda-cute-wave-gif-16214706', 'https://tenor.com/view/hello-there-baby-yoda-mandolorian-hello-gif-20136589', 'https://tenor.com/view/baby-yoda-gif-19161736'))}")
 
     # Dice Example command: 1d6 - rolls 1 dice, with the highest value being 6
     @command(name="dice", aliases=["roll"])
