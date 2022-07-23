a90695e999e9544552f02131bc8abd06c80d6d35
Henry <Henry_Koleoso@hotmail.co.uk>
2022-07-23 14:59:11 +0100

    work in progress help command

--- lib/cogs/help.py
+++ lib/cogs/help.py
@@ -2,6 +2,7 @@ from typing import Optional
 
 from discord import Embed
 from discord.utils import get
+from discord.ext.menus import MenuPages, ListPageSource
 from discord.ext.commands import Cog
 from discord.ext.commands import command
 
@@ -16,6 +17,33 @@ def syntax(command):
     params = " ".join(params)
 
     return f"```{cmd_and_aliases} {params}```"
+
+
+class HelpMenu(ListPageSource):
+    def __int__(self, ctx, data):
+        self.ctx = ctx
+
+        super().__init__(data, per_page=3)
+
+    async def write_page(self, menu, fields=[]):
+        offset = (menu.current_page * self.per_page) + 1
+        len_data = len(self.entries)
+
+        embed = Embed(title="Help", description="Welcome to Baby Yoda help dialog", colour=self.ctx.author.colour)
+        embed.set_thumbnail(url=self.ctx.guild.me.avatar_url)
+        embed.set_footer(text=f"{offset:,} - {min(len_data, offset + self.per_page - 1):,} of {len_data:,} commands")
+
+        for name, value in fields:
+            embed.add_field(name=name, value=value, inline=False)
+        return embed
+
+    async def format_page(self, menu, entries):
+        fields = []
+
+        for entry in entries:
+            fields.append((entry.brief or "No Description", syntax(entry)))
+
+        return await self.write_page(menu, fields)
 
 
 class Help(Cog):
@@ -34,7 +62,10 @@ class Help(Cog):
     async def show_help(self, ctx, cmd: Optional[str]):
         """Shows this message"""
         if cmd is None:
-            pass
+            menu = MenuPages(source=HelpMenu(ctx, list(self.bot.commands)),
+                             clear_reactions_after=True,
+                             timeout=60.0)
+            await menu.start(ctx)
 
         else:
             if (command := get(self.bot.commands, name=cmd)):
