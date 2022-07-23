a79a91b2b5d9a13e625e1daf7c2cdc57449490ec
Henry <Henry_Koleoso@hotmail.co.uk>
2022-07-23 20:37:33 +0100

    finish creating welcoming members and sending them a dm

--- lib/cogs/welcome.py
+++ lib/cogs/welcome.py
@@ -15,21 +15,23 @@ class Welcome(Cog):
             self.bot.cogs_ready.ready_up("welcome")
 
     @Cog.listener()
-    async def member_join(self, member):
+    async def on_member_join(self, member):
         db.execute("INSERT INTO exp (UserID) VALUES (?)", member.id)
-        await self.bot.get_channel(999416235609555126).send(f"Welcome to **{member.guild.name}**, {member.mention}!")
+        await self.bot.get_channel(999416235609555126).send(f"Welcome to **{member.guild.name}** {member.mention}!")
 
         try:
-            await member.send(f"Welcome to {member.guild.name}!")  # send a dm
+            await member.send(f"Welcome to **{member.guild.name}**! Enjoy your stay!")
 
         except Forbidden:
             pass
 
-        await member.add_roles(*(member.guild.get_role(id_) for id_ in (706479484781723679, 1000477037951193228)))
+        # make sure these roles are not at the top - above any admin roles or there will be an error
+        await member.add_roles(member.guild.get_role(1000485792264753215), member.guild.get_role(1000477037951193228))
 
     @Cog.listener()
     async def on_member_remove(self, member):
-        pass
+        db.execute("DELETE FROM exp WHERE UserID = ?", member.id)
+        await self.bot.get_channel(999416235609555126).send(f"{member.display_name} has left {member.guild.name}.")
 
 
 def setup(bot):
