cad7500f666fa14bcea5ec39aba2b83b4be8ce6b
Henry <Henry_Koleoso@hotmail.co.uk>
2022-07-23 20:40:48 +0100

    add fun gif to send dm to new members

--- lib/cogs/welcome.py
+++ lib/cogs/welcome.py
@@ -1,6 +1,7 @@
 from discord import Forbidden
 from discord.ext.commands import Cog
 from discord.ext.commands import command
+from random import choice
 
 from ..db import db
 
@@ -20,7 +21,8 @@ class Welcome(Cog):
         await self.bot.get_channel(999416235609555126).send(f"Welcome to **{member.guild.name}** {member.mention}!")
 
         try:
-            await member.send(f"Welcome to **{member.guild.name}**! Enjoy your stay!")
+            await member.send(f"Welcome to **{member.guild.name}**!")
+            await member.send(f"{choice(('https://tenor.com/view/the-mandalorian-baby-yoda-the-child-star-wars-cute-gif-16181871', 'https://tenor.com/view/baby-yoda-hi-hello-greet-wave-gif-15912640', 'https://tenor.com/view/the-mandalorian-the-child-baby-yoda-cute-wave-gif-16214706', 'https://tenor.com/view/hello-there-baby-yoda-mandolorian-hello-gif-20136589', 'https://tenor.com/view/baby-yoda-gif-19161736'))}")
 
         except Forbidden:
             pass
