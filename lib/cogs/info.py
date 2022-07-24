72d8c8773706ac2fe24cf64eb061e6549eeb8e1a
Henry <Henry_Koleoso@hotmail.co.uk>
2022-07-24 21:36:16 +0100

    edit

--- lib/cogs/info.py
+++ lib/cogs/info.py
@@ -20,15 +20,15 @@ class Info(Cog):
         embed.set_thumbnail(url=target.avatar_url)
         # embed.set_thumbnail(url=ctx.guild.icon_url) use icon of the discord server
 
-        fields = [("ID", target.id, False),
-                  ("Name", str(target), True),
+        fields = [("Name", str(target), True),
+                  ("ID", target.id, True),
                   ("Bot", target.bot, True),
                   ("Top role", target.top_role.mention, True),
                   ("Status", str(target.status).title(), True),
                   ("Activity", f"{str(target.activity.type).split('.')[-1].title() if target.activity else 'N/A'} {target.activity.name if target.activity else ''}", True),
                   ("Created at", target.created_at.strftime("%d/%m/%Y %H:%M:%S"), True),
                   ("Joined at", target.joined_at.strftime("%d/%m/%Y %H:%M:%S"), True),
-                  ("Boosted", bool(target.premium_since), True)]
+                  ("Boosted", bool(target.premium_since), True),]
 
         for name, value, inline in fields:
             embed.add_field(name=name, value=value, inline=inline)
