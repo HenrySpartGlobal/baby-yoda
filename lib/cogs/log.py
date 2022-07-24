42c4cf9aab2084cba0850c7ea4808b00d87f7f99
Henry <Henry_Koleoso@hotmail.co.uk>
2022-07-24 20:42:03 +0100

    add update on: descriminator, username, display picture, editing message and deleting messages

--- lib/cogs/log.py
+++ lib/cogs/log.py
@@ -12,13 +12,38 @@ class Log(Cog):
     @Cog.listener()
     async def on_ready(self):
         if not self.bot.ready:
-            self.log_channel = self.bot.get_channel(1000527671597486161)
+            self.log_channel = self.bot.get_channel(1000527671597486161)  # get this from database for multi server
             self.bot.cogs_ready.ready_up("log")
 
     @Cog.listener()
     async def on_user_update(self, before, after):
+        if before.name != after.name:
+            embed = Embed(title=f"{before.author.display_name} changed their Username",
+                          colour=after.colour,
+                          timestamp=datetime.utcnow())
+            embed.set_thumbnail(url=before.avatar_url)
+
+            fields = [("Before", before.name, False), ("After", after.name, False)]
+
+            for name, value, inline in fields:
+                embed.add_field(name=name, value=value, inline=inline)
+
+        if before.discriminator != after.discriminator:
+            embed = Embed(title=f"{before.author.display_name} changed their Discriminator",
+                          colour=after.colour,
+                          timestamp=datetime.utcnow())
+            embed.set_thumbnail(url=before.avatar_url)
+
+            fields = [("Before", before.discriminator, False), ("After", after.discriminator, False)]
+
+            for name, value, inline in fields:
+                embed.add_field(name=name, value=value, inline=inline)
+
+            await self.log_channel.send(embed=embed)
+
         if before.avatar_url != after.avatar_url:
-            embed = Embed(title="Display picture Update", description="Avatar change - New image below",
+            embed = Embed(title=f"{before.author.display_name} Updated their picture",
+                          description="Avatar change - New image below",
                           colour=after.colour,
                           timestamp=datetime.utcnow())
 
@@ -30,10 +55,10 @@ class Log(Cog):
     @Cog.listener()
     async def on_member_update(self, before, after):
         if before.display_name != after.display_name:
-            embed = Embed(title="Member Update", description="Nickname change",
+            embed = Embed(title=f"{before.author.display_name} Nickname changed",
                           colour=after.colour,
                           timestamp=datetime.utcnow())
-            # embed.set_thumbnail(url=before.avatar_url)
+            embed.set_thumbnail(url=before.avatar_url)
 
             fields = [("Before", before.display_name, False), ("After", after.display_name, False)]
 
@@ -42,15 +67,51 @@ class Log(Cog):
 
             await self.log_channel.send(embed=embed)
 
-    @Cog.listener()
-    async def on_member_edit(self, before, after):
-        if not after.author.bot:
-            pass
+        elif before.roles != after.roles:
+            embed = Embed(title=f"{before.author.display_name} Role changed",
+                          colour=after.colour,
+                          timestamp=datetime.utcnow())
+            embed.set_thumbnail(url=before.avatar_url)
+
+            fields = [("Before", ", ".join([r.mention for r in before.roles]), False),
+                      ("After", ", ".join([r.mention for r in after.roles]), False)]
+
+            for name, value, inline in fields:
+                embed.add_field(name=name, value=value, inline=inline)
+
+            await self.log_channel.send(embed=embed)
 
     @Cog.listener()
-    async def on_member_delete(self, before, after):
+    async def on_message_edit(self, before, after):
         if not after.author.bot:
-            pass
+            if before.content != after.content:
+                embed = Embed(title=f"{after.author.display_name} edited their message",
+                              colour=after.author.colour,
+                              timestamp=datetime.utcnow())
+                embed.set_thumbnail(url=before.author.avatar_url)
+
+                fields = [("Before", before.content, False),
+                          ("After", after.content, False)]
+
+                for name, value, inline in fields:
+                    embed.add_field(name=name, value=value, inline=inline)
+
+                await self.log_channel.send(embed=embed)
+
+    @Cog.listener()
+    async def on_message_delete(self, message):
+        if not message.author.bot:
+            embed = Embed(title=f"{message.author.display_name} deleted their message",
+                          colour=message.author.colour,
+                          timestamp=datetime.utcnow())
+            embed.set_thumbnail(url=message.author.avatar_url)
+
+            fields = [("Content", message.content, False)]
+
+            for name, value, inline in fields:
+                embed.add_field(name=name, value=value, inline=inline)
+
+            await self.log_channel.send(embed=embed)
 
 
 def setup(bot):
