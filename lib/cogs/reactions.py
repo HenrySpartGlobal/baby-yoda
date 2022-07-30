6b8b0d5186e91187e48ad64223b275404e425c48
Henry <Henry_Koleoso@hotmail.co.uk>
2022-07-30 23:41:18 +0100

    wip working with reactions

--- lib/cogs/reactions.py
+++ lib/cogs/reactions.py
@@ -12,21 +12,21 @@ class Reactions(Cog):
 
     @Cog.listener()
     async def on_reaction_add(self, reaction, user):
-        print(f"{user.display_name} reacted with {reaction.emoji.name}")
+        print(f"{user.display_name} reacted with {reaction.emoji}")
 
     @Cog.listener()
     async def on_reaction_remove(self, reaction, user):
-        print(f"{user.display_name} removed their reaction of {reaction.emoji.name}")
+        print(f"{user.display_name} removed their reaction of {reaction.emoji}")
 
     @Cog.listener()
     async def on_raw_reaction_add(self, payload):
-        print(f"[RAW] {payload.member.display_name} reacted with {payload.emoji.name}")
+        print(f"[RAW] {payload.member.display_name} reacted with {str(payload.emoji.name)}")
 
     @Cog.listener()
     async def on_raw_reaction_remove(self, payload):
         member = self.bot.guild.get_member(payload.user_id)
-        print(f"[RAW] {member.display_name} removed their reaction of {payload.emoji.name}")
+        print(f"[RAW] {member.display_name} removed their reaction of {str(payload.emoji.name)}")
 
 
 def setup(bot):
-    bot.add_cogs(Reactions(bot))
+    bot.add_cog(Reactions(bot))
