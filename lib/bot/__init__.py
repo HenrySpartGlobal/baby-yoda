af4cb1cb6d3fa3a83ef172e181eb34d14a0efb2e
Henry <31442053+HenrySpartGlobal@users.noreply.github.com>
2022-08-05 14:16:32 +0100

    Update __init__.py

--- lib/bot/__init__.py
+++ lib/bot/__init__.py
@@ -149,7 +149,7 @@ class Bot(BotBase):
             while not self.cogs_ready.all_ready():
                 await sleep(0.5)
 
-            await self.stdout.send("Baby Yoda is now Online! - Ready to Go!")
+            await self.stdout.send("Baby Yoda Bot is now Online!!")
             self.ready = True
             print("Bot ready")
 
